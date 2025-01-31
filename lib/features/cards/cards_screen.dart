daf57edbc6ca110dbdfc9bf03d04208ac9528579
Matheus Clemente <matheus_klementt@hotmail.com>
2025-01-31 19:31:46 -0300

    Add ListView

--- lib/features/cards/cards_screen.dart
+++ lib/features/cards/cards_screen.dart
@@ -8,8 +8,15 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Golpes Comuns'),
       ),
-      body: const Center(
-        child: Placeholder(),
+      body: Center(
+        child: ListView.separated(
+          itemCount: 6,
+          itemBuilder: (BuildContext context, int index) {
+            return const Placeholder(fallbackHeight: 50);
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+        ),
       ),
     );
   }
