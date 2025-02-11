7f5fe281e70c0008ffb340ba8460d99f3d5c21b0
Matheus Clemente <matheus_klementt@hotmail.com>
2025-02-11 17:40:02 -0300

    Update message_analyzer_screen.dart

--- lib/features/message_analyzer/message_analyzer_screen.dart
+++ lib/features/message_analyzer/message_analyzer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:protetor_digital_prototipo/i18n/strings.g.dart';
 
 class MessageAnalyzerScreen extends StatelessWidget {
   const MessageAnalyzerScreen({super.key});
@@ -6,10 +7,33 @@ class MessageAnalyzerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Análise de Mensagens'),
+        title: Text(t.messageAnalyzerScreen.title),
       ),
-      body: const Center(
-        child: Placeholder(),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Text("Cole abaixo a mensagem suspeita:"),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
+                height: 300,
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  expands: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Verificar"),
+            )
+          ],
+        ),
       ),
     );
   }
